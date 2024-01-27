7472fedde9ccc4f18ce1e57917a8dfeee7b46e2d
Stephan Rudenskyi <s.rudenskiy@gmail.com>
2024-01-27 20:50:47 +0100

    annotation in DeveloperController class

--- src/main/java/org/example/taskmanagement/controller/DeveloperController.java
+++ src/main/java/org/example/taskmanagement/controller/DeveloperController.java
@@ -21,11 +21,32 @@ public class DeveloperController {
         this.developerService = developerService;
     }
 
+    /**
+     * RESTful endpoint to create a new developer.
+     * <p>
+     * This endpoint allows clients to create a new developer by providing the necessary information
+     * in the request body. The information should be in the form of a JSON object. The created developer
+     * is returned as a response in the form of a {@link DeveloperDto}.
+     *
+     * @param developerDto The developer details provided in the request body.
+     * @return A {@link DeveloperDto} represent enw created developer.
+     */
     @PostMapping
     public DeveloperDto createDeveloper(@RequestBody DeveloperDto developerDto) {
         return developerService.createDeveloper(developerDto);
     }
 
+    /**
+     * RESTful endpoint to retrieve a developer by their unique identifier.
+     * <p>
+     * This endpoint allows clients to retrieve details of a specific developer by providing
+     * the developer's ID as a path variable. If the developer with the specified ID exists,
+     * their details are returned as a response in the form of a {@link DeveloperDto}.
+     *
+     * @param developerId The unique identifier of the developer to be retrieved.
+     * @return A {@link DeveloperDto} represent the details of the requested developer
+     * @throws jakarta.persistence.EntityNotFoundException if developer with provided developerId was not found
+     */
     @GetMapping("/{developerId}")
     public DeveloperDto getDeveloperById(@PathVariable Long developerId) {
         return developerService.getDeveloperById(developerId);
